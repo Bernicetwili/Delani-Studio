115aefe9e6eb74307f85417bdd1035b76e899516
Bernice Twili <bernicetwili0@gmail.com>
2021-01-06 06:18:28 +0300

    add about,service,portfolio

---
+++ Js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  ;
+});
+$(document).ready(function() {
+    $(".clickable").click(function() {
+        $("#show").toggle();
+        $("#hide").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".clickable2").click(function() {
+        $("#show2").toggle();
+        $("#hide2").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".clickable3").click(function() {
+        $("#show3").toggle();
+        $("#hide3").toggle();
+    });
+});
+
