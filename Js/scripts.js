f3159e63a2f7858c53a729df3aaffcf0abfcb058
Bernice Twili <bernicetwili0@gmail.com>
2021-01-07 09:17:40 +0300

    try resolving image

--- Js/scripts.js
+++ Js/scripts.js
@@ -24,13 +24,8 @@ $(document).ready(function () {
       event.preventDefault();
       var name = $("#name").val();
       let name = $("#name").val();
-​
-       if ($("#name").val()) {
         alert("Hey " + name + ",thank you for reaching out to us,We will get back to you");
         
-        else {
-        alert("Please provide your correct name and email!");
-        }
-  
+        
     });
 });
