777982758717c0c375e877e62a24a5c428eef0fc
Bernice Twili <bernicetwili0@gmail.com>
2021-01-07 08:26:40 +0300

    adding jquery

--- Js/scripts.js
+++ Js/scripts.js
@@ -19,4 +19,18 @@ $(document).ready(function() {
         $("#hide3").toggle();
     });
 });
-$()
+$(document).ready(function () {
+    $("#my-form").submit(function (event) {
+      event.preventDefault();
+      var name = $("#name").val();
+      let name = $("#name").val();
+​
+       if ($("#name").val()) {
+        alert("Hey " + name + ",thank you for reaching out to us,We will get back to you");
+        
+        else {
+        alert("Please provide your correct name and email!");
+        }
+  
+    });
+});
